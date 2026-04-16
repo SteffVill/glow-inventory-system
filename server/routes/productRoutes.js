ad249a05a5ab48f8021568a8b955667fb59a9398
SteffVill <a.villalba.soles@gmail.com>
2026-04-16 11:31:20 -0400

    feat: add GET endpoint to retrieve all products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -11,4 +11,13 @@ router.post('/add', async (req, res) => {
     }
 });
 
+router.get('/all', async (req, res) => {
+    try {
+        const products = await Product.findAll(); 
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ mensaje: "Error al obtener los productos", error });
+    }
+});
+
 module.exports = router;
