ca2f184bf735679e277901e36949aa429cffdca2
SteffVill <a.villalba.soles@gmail.com>
2026-04-16 12:27:32 -0400

    chore: install and configure CORS in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('./config/db');
 const Product = require('./models/Product'); 
 const app = express();
 const PORT = 5000;
 const productRoutes = require('./routes/productRoutes');
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes); 
 
