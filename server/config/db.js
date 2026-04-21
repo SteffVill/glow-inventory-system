a106c69dd855b63dace37e9027ac10798037a440
SteffVill <a.villalba.soles@gmail.com>
2026-04-21 18:00:07 -0400

    fix: added port and timeout to sequelize connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,24 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-
 const db = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASS, 
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
-        logging: false 
+        logging: false,
+        dialectOptions: {
+            connectTimeout: 60000 
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 60000,
+            idle: 10000
+        }
     }
 );
 
